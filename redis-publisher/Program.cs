6185b368b7d61b89f3d3159507266fa476768ba0
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-05-22 04:18:44 +0300

    completed...

---
+++ redis-publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace redis_publisher
+{
+    class Program
+    {
+        private const string RedisConnectionString = "localhost:6379";
+        private static ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(RedisConnectionString);
+        private const string Channel = "test-channel";
+
+        static void Main(string[] args)
+        {
+            var pubsub = connection.GetSubscriber();
+            
+            pubsub.PublishAsync(Channel, "This is a test message!!", CommandFlags.FireAndForget);
+            Console.Write("Message Successfully sent to test-channel");
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ redis-subscriber/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace redis_subscriber
+{
+    class Program
+    {
+        private const string RedisConnectionString = "localhost:6379";
+        private static ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(RedisConnectionString);
+        private const string Channel = "test-channel";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listening test-channel");
+            var pubsub = connection.GetSubscriber();
+
+            pubsub.Subscribe(Channel, (channel, message) => Console.Write("Message received from test-channel : " + message));
+            Console.ReadLine();
+        }
+    }
+}
